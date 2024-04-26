4e66a9f5418374bb6bcb62ca9d2b37e799309caa
ArtyomAbrosimov <142402743+ArtyomAbrosimov@users.noreply.github.com>
2024-04-26 16:14:35 +0300

    Add files via upload

--- algorithms.h
+++ algorithms.h
@@ -10,7 +10,7 @@ namespace groebner {
         using SetOfPolynomials = SetOfPolynomials<T, Order>;
         using Term = Term<T>;
 
-        static Polynomial MakeSPolynom(const Polynomial &first, const Polynomial &second) {
+        static Polynomial MakeSPolynomial(const Polynomial &first, const Polynomial &second) {
             Term leading1 = first.GetLeading();
             Term leading2 = second.GetLeading();
             Term lcm = LCM(leading1, leading2);
@@ -29,26 +29,45 @@ namespace groebner {
             return res;
         }
 
-        static SetOfPolynomials DoBuchberger(const SetOfPolynomials &set_of_polynomials) {
-            if (set_of_polynomials.GetSize() == 0) { // добавить оптимизацию, сделать редуцированный
-                return {};
-            }
-            SetOfPolynomials res;
-            res.AddPolynomial(*set_of_polynomials.begin());
-            for (const Polynomial &polynomial: set_of_polynomials) {
-                if (!ReducePolynomial(polynomial, res).IsZero()) {
-                    res.AddPolynomial(polynomial);
-                }
-            }
-            for (int i = 0; i < res.GetSize(); ++i) {
-                for (int j = 0; j < i; ++j) {
-                    Term gcd_of_leadings = GCD(res[i].GetLeading(), res[j].GetLeading());
-                    if (gcd_of_leadings.GetMonomial() == Monomial()) continue;
-                    Polynomial s_polynom = MakeSPolynom(res[i], res[j]);
+        static SetOfPolynomials DoSlowBuchberger(const SetOfPolynomials &set_of_polynomials) {
+            SetOfPolynomials res = set_of_polynomials;
+            for (VectorIndex i = 0; i < res.GetSize(); ++i) {
+                for (VectorIndex j = 0; j < i; ++j) {
+                    Polynomial s_polynom = MakeSPolynomial(res[i], res[j]);
                     Polynomial reduced = ReducePolynomial(s_polynom, res);
                     if (!reduced.IsZero()) res.AddPolynomial(reduced);
                 }
             }
+            DoAllReductions(res);
+            return res;
+        }
+
+        static SetOfPolynomials DoBuchberger(const SetOfPolynomials &set_of_polynomials) {
+            std::vector<std::pair<VectorIndex, VectorIndex>> indexes;
+            VectorIndex last = set_of_polynomials.GetSize() - 1;
+            for (VectorIndex i = 0; i <= last; ++i) {
+                for (VectorIndex j = i + 1; j <= last; ++j) {
+                    indexes.push_back({i, j});
+                }
+            }
+            SetOfPolynomials res = set_of_polynomials;
+            while (!indexes.empty()) {
+                auto &[i, j] = indexes.back();
+                indexes.pop_back();
+                if (LCM(res[i].GetLeading(), res[j].GetLeading()) == res[i].GetLeading() * res[j].GetLeading() ||
+                    CheckOptimization(i, j, res, indexes)) {
+                    continue;
+                }
+                Polynomial s_polynom = MakeSPolynomial(res[i], res[j]);
+                Polynomial reduced = ReducePolynomial(s_polynom, res);
+                if (reduced.IsZero()) continue;
+                last += 1;
+                res.AddPolynomial(reduced);
+                for (VectorIndex l = 0; l < last; ++l) {
+                    indexes.push_back({l, last});
+                }
+            }
+            DoAllReductions(res);
             return res;
         }
 
@@ -59,10 +78,40 @@ namespace groebner {
         }
 
     private:
+        static bool CheckOptimization(VectorIndex i, VectorIndex j, const SetOfPolynomials &res, const
+        std::vector<std::pair<VectorIndex, VectorIndex>> &indexes) {
+            for (VectorIndex l = 0; l < indexes.size(); ++l) {
+                if (l != i && l != j &&
+                    std::find(indexes.begin(), indexes.end(), std::make_pair(i, l)) == indexes.end() &&
+                    std::find(indexes.begin(), indexes.end(), std::make_pair(j, l)) == indexes.end() &&
+                    res[l].GetLeading().Divides(LCM(res[i].GetLeading(), res[j].GetLeading()))) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static void DoAllReductions(SetOfPolynomials &set_of_polynomials) {
+            for (VectorIndex i = 0; i < set_of_polynomials.GetSize(); ++i) {
+                for (VectorIndex j = 0; j < set_of_polynomials.GetSize(); ++j) {
+                    if (i == j) continue;
+                    Polynomial res = ReducePolynomial(set_of_polynomials[i], set_of_polynomials[j]);
+                    if (res == set_of_polynomials[i]) continue;
+                    set_of_polynomials.ChangePolynomial(i, res);
+                    if (res.IsZero()) set_of_polynomials.ErasePolynomial(i);
+                    return DoAllReductions(set_of_polynomials);
+                }
+            }
+            for (VectorIndex i = 0; i < set_of_polynomials.GetSize(); ++i) {
+                Polynomial polynomial = set_of_polynomials[i];
+                polynomial /= polynomial.GetLeading().GetCoefficient();
+                set_of_polynomials.ChangePolynomial(i, polynomial);
+            }
+        }
+
         static void DoReduction(Polynomial &res, const Polynomial &reducing) {
             const Term &leading = reducing.GetLeading();
             for (const Term &term: res) {
-                std::cout << term << ": " << leading << "\n";
                 if (leading.GetMonomial().Divides(term.GetMonomial())) {
                     Term quotient = term / leading;
                     Polynomial subtrahend = reducing * quotient;
--- main.cpp
+++ main.cpp
@@ -1,43 +1,6 @@
-#include "algorithms.h"
-
-namespace groebner {
-    template<typename T, typename Order>
-    SetOfPolynomials<T, Order> Cyclic(int n) {
-        SetOfPolynomials<T, Order> res;
-        for (int i = 1; i < n; ++i) { // номер полинома
-            Polynomial<T, Order> poly;
-            for (int j = 0; j < n; ++j) { // номер монома
-                std::unordered_map<groebner::Index, groebner::Degree> a;
-                for (int k = 0; k < i; ++k) { // степени
-                    a.insert({(j + k) % n, 1});
-                }
-                groebner::Monomial A(std::move(a));
-                groebner::Term<int> ta({A, 1});
-                poly += ta;
-            }
-            res.AddPolynomial(poly);
-        }
-        std::unordered_map<groebner::Index, groebner::Degree> a;
-        for (int k = 0; k < n; ++k) {
-            a.insert({k, 1});
-        }
-        Polynomial<T, Order> poly;
-        groebner::Monomial A(std::move(a));
-        groebner::Term<int> ta({A, 1});
-        groebner::Monomial B;
-        groebner::Term<int> tb({B, -1});
-        poly += ta;
-        poly += tb;
-        res.AddPolynomial(poly);
-        return res;
-    }
-}
+#include "tests/tests.h"
 
 int main() {
-    groebner::SetOfPolynomials c =
-            groebner::Cyclic<int, groebner::GraduatedLexicographicalOrder>(5);
-    std::cout << c << "\n";
-    groebner::SetOfPolynomials basis =
-            groebner::Algorithms<int, groebner::GraduatedLexicographicalOrder>::DoBuchberger(c);
-    std::cout << basis << "\n";
+    groebner::RunTests(4);
+    return 0;
 }
